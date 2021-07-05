fa01070a5b7b0f3893ed4cb22af992ef27522952
andrelsilvar <andre.silva@saude.ce.gov.br>
2021-07-05 18:22:37 -0300

    Prepare to deploy

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+   "type": "postgres",
+   "url": process.env.DATABASE_URL,
+   "logging": false,
+   "entities": [
+      "src/entities/**/*.ts",
+      "dist/entities/**/*.js"
+   ],
+   "migrations": [
+      "src/database/migrations/**/*.ts",
+      "dist/database/migrations/**/*.js"
+   ],
+   "cli": {
+      "migrationsDir": "src/database/migrations",
+      "entitiesDir": "src/entities"
+   }
+}
