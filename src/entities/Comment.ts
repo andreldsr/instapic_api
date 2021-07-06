8912aa664facffabb4b8a2aec928526a726f5cab
andrelsilvar <andre.silva@saude.ce.gov.br>
2021-07-06 18:20:12 -0300

    :label: Add username
    Add username to comment endpoint

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,5 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from 'uuid'
+import { User } from "./User";
 
 @Entity("comments")
 export class Comment {
@@ -11,6 +12,9 @@ export class Comment {
     photoId: string
     @Column({ name: "user_id" })
     userId: string
+    @JoinColumn({ name: "user_id", referencedColumnName: "id" })
+    @ManyToOne(() => User)
+    user: User
     @CreateDateColumn({ name: "comment_date" })
     date: Date
 
--- src/services/photo/PhotoCommentService.ts
+++ src/services/photo/PhotoCommentService.ts
@@ -1,14 +1,29 @@
 import { getCustomRepository } from 'typeorm';
 import { CommentRepository } from '../../repositories/CommentRepository';
 
-
+interface CommentDTO {
+    date: Date;
+    text: string;
+    userName: string;
+}
 class PhotoCommentService {
     async execute(photoId: string) {
         const commentRepository = getCustomRepository(CommentRepository)
         const comments = await commentRepository.find({
-            photoId
+            where: {
+                photoId
+            },
+            join: {
+                alias: "comment",
+                leftJoinAndSelect: {
+                    user: "comment.user"
+                }
+            }
         })
-        return comments
+        const commentsDTO: CommentDTO[] = comments.map<CommentDTO>((comment): CommentDTO => {
+            return { date: comment.date, text: comment.text, userName: comment.user.userName }
+        })
+        return commentsDTO
     }
 }
 
